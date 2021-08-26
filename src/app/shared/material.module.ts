563f45d8b859b21ec0b8c260b050a2fef9d6bdc8
Wojciech Dudek <wdudek82@gmail.com>
2021-08-26 23:47:40 +0200

    master: add a datepicker

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,9 +3,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+
+const matModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+];
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [...matModules],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
+  exports: [...matModules],
 })
 export class MaterialModule {}
